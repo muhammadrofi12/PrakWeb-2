080186854dbdc0f36f9a492032c85ab989f9aaec
Muhammad Rofi <112401116+muhammadrofi12@users.noreply.github.com>
2023-04-02 01:17:50 +0700

    update heading

--- src/jenis_produk.php
+++ src/jenis_produk.php
@@ -9,7 +9,7 @@ $data_produk = $models->tampil_jenisProduk();
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1 class="m-0">Dashboard</h1>
+                    <h1 class="m-0">Jenis Produk</h1>
                 </div><!-- /.col -->
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
