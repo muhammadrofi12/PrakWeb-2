17accf1f199f05cb7fac7aeba3e1889d6a54f43c
Muhammad Rofi <112401116+muhammadrofi12@users.noreply.github.com>
2023-04-02 01:19:47 +0700

    update heading

--- src/pembelian.php
+++ src/pembelian.php
@@ -9,7 +9,7 @@ $data_produk = $models->tampil_pembelian();
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1 class="m-0">Dashboard</h1>
+                    <h1 class="m-0">Daftar Pembeli</h1>
                 </div><!-- /.col -->
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
